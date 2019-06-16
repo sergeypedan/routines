6162be04c4f381ac5e430ec1f54d70cbda66f855
Sergey Pedan <sergey.pedan@gmail.com>
2019-06-17 01:52:27 +0700

    Creates importer

---
+++ lib/tasks/import/all.rake
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+namespace :import do
+	task :all, environment: true do
+
+		resources = [
+			"muscles",
+			"excercises",
+			"excercisings",
+			"habits",
+			"habit_entries",
+			"association_excercise_muscles"
+		]
+
+		puts "Will import:"
+		resources.each do |resource|
+			puts "• #{resource}"
+		end
+		puts
+
+		resources.each do |resource|
+			YamlDb::Importer.new(resource: resource).import!
+		end
+
+	end
+end
---
+++ lib/yaml_db/importer.rb
@@ -0,0 +1,55 @@
+module YamlDb
+	class Importer
+
+		# @dir is directory name
+		# @resource like "habits"
+		#
+		def initialize(resource:, dir: "export")
+			@resource = resource
+			@dir = dir
+		end
+
+
+		def import!
+			puts "\nImporting #{resource}..."
+
+			if klass.count.positive?
+				puts "#{klass} has records. Deleting them."
+				klass.destroy_all
+			end
+
+			records.each do |record|
+				klass.create! record_hash(record)
+			end
+			puts "#{resource} imported\n\n"
+		end
+
+
+		private def columns
+			yaml["columns"]
+		end
+
+		private def file_path
+			Rails.root.join("db", @dir, "#{@resource}.yml")
+		end
+
+		private def klass
+			@klass ||= @resource.singularize.camelize.constantize
+		end
+
+		private def record_hash(record)
+			fail ArgumentError, "`record` must be an Array" unless record.is_a? Array
+			fail StandardError, "`record` length does not match `columns` length" unless record.size == columns.size
+			Hash[columns.zip(record)]
+		end
+
+		private def records
+			@records ||= yaml["records"]
+		end
+
+		private def yaml
+			@yaml ||= YAML.load_file(file_path)[@resource]
+		end
+
+	end
+end
