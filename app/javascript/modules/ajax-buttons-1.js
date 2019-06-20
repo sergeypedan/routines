30ddf2e0e5dd6df947981b96fdd7d7218b0d265e
Sergey Pedan <sergey.pedan@gmail.com>
2019-06-20 20:57:11 +0700

    Better Ajax button events

--- app/javascript/modules/ajax-buttons-1.js
+++ app/javascript/modules/ajax-buttons-1.js
@@ -2,5 +2,9 @@
 
 
 $(document).on("click", '[data-behavior="ajax-btn-1"]', function() {
-	this.innerHTML = '<span class="fa fa-refresh fa-spin"></span>'
+	// this.disabled  = true // тогда не срабатывает UJS
+	this.innerHTML = "🕒" // '<span class="fa fa-refresh fa-spin"></span>'
+	this.classList.remove("increment-btn-2--success")
+	this.classList.remove("increment-btn-2--danger")
+	this.classList.add("increment-btn-2--waiting")
 })
