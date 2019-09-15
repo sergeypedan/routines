0a474b2ea2cc4ab00ba16dc58c7042814b7e4fed
Sergey Pedan <sergey.pedan@gmail.com>
2019-09-15 18:13:43 +0300

    Duplicates with current date

--- app/controllers/excercisings_controller.rb
+++ app/controllers/excercisings_controller.rb
@@ -46,9 +46,10 @@ class ExcercisingsController < DashboardsController
 
 
 	def duplicate
-		source_record = Excercising.find params[:id]
-		attributes    = source_record.attributes.except("id", "created_at", "updated_at")
-		new_record    = Excercising.new(attributes)
+		source_record   = Excercising.find params[:id]
+		attributes      = source_record.attributes.except("id", "created_at", "updated_at")
+		attributes.date = Time.current.to_date
+		new_record      = Excercising.new(attributes)
 		new_record.save
 		redirect_to excercisings_path, notice: "Duplicated!"
 	end
