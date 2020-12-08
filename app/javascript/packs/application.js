d34dc9c2b7b4f362f9231600a83d02c0068ce4f6
Sergey Pedan <sergey.pedan@gmail.com>
2020-12-08 14:24:00 +0700

    Corrects collapsing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,11 @@ Rails.start()
 
 // Bootstrap
 
-// import "bootstrap/dist/js/bootstrap.js"
-// import "bootstrap/js/dist/util.js"
-// import "bootstrap/js/dist/alert.js"
-// import "bootstrap/js/dist/button.js"
-// import "bootstrap/js/dist/collapse.js"
+import "bootstrap/dist/js/bootstrap.js"
+import "bootstrap/js/dist/util.js"
+import "bootstrap/js/dist/alert.js"
+import "bootstrap/js/dist/button.js"
+import "bootstrap/js/dist/collapse.js"
 
 
 // App modules
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	devise :database_authenticatable, :rememberable, :validatable
+
 end
 
 # == Schema Information
