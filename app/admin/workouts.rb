eb0f8b66c053e8d088e406421fc481f7ec3ab5ae
Sergey Pedan <sergey.pedan@gmail.com>
2021-07-10 01:14:25 +0700

    Adds workout to AA

---
+++ app/admin/workouts.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Workout do
+
+	menu parent: "Sport"
+
+	permit_params :date, :excercise_id, :repetitions_count, :resistance_duration, :weight
+
+	includes :excercise
+
+	index do
+		selectable_column
+		id_column
+
+		column :date
+		column :excercise
+		column :repetitions_count
+		column :resistance_duration
+		column :weight
+
+		actions
+	end
+
+end
