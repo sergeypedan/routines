d5ba6acc3fcf29db1e2a1518b50fa1d75f7c3f8f
Sergey Pedan <sergey.pedan@gmail.com>
2021-08-31 20:08:03 +0700

    Manual workout form

--- app/admin/workouts.rb
+++ app/admin/workouts.rb
@@ -15,11 +15,26 @@ ActiveAdmin.register Workout do
 		column :date
 		column :excercise
 		column :repetitions_count
-		column :repetitions_count
 		column :resistance_duration
 		column :weight
 
 		actions
+	end
+
+
+	form do |f|
+		f.inputs do
+			f.input :date
+			f.input :excercise
+		end
+
+		f.inputs do
+			f.input :repetitions_count
+			f.input :resistance_duration
+			f.input :weight
+		end
+
+		f.actions
 	end
 
 end
